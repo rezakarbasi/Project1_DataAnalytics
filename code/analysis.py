6d3f54b5d3bf223ec51df344a082dd386b07f80e
reza <a.rz.karbasi@gmail.com>
2021-01-03 10:37:10 +0330

    presentation added + code updated

--- code/analysis.py
+++ code/analysis.py
@@ -5,7 +5,8 @@ import seaborn as sns
 
 %matplotlib inline
 
-data = pd.read_csv("investments_VC/investments_VC.csv",encoding="ANSI")
+path='G:/PersonalFiles/Projects/1_DarCProj/Data Analytics/Project1_DataAnalytics/'
+data = pd.read_csv(path+"Data/investments_VC.csv",encoding="ANSI")
 data.dropna(axis=0,how='all',inplace=True)
 data.dropna(axis=1,how='all',inplace=True)
 
@@ -27,8 +28,8 @@ plt.savefig('#startups.png')
 
 #%%
 startYear=2000
-endYear = 2009
-thresh = 60
+endYear = 2013
+thresh = 100
 
 df={}
 for year,group in data.groupby('founded_year'):
@@ -41,10 +42,10 @@ a=df[np.sum(df>thresh,axis=1)>0]
 print(len(a))
 
 sns.heatmap(a,linewidths=.1)
-plt.title("areas of statrtups "+str(startYear)+"-"+str(endYear))
+plt.title("# of statrtups in best areas - "+str(startYear)+"-"+str(endYear))
 plt.xlabel('year')
 plt.ylabel('areas of startups')
-plt.savefig("areas-"+str(endYear+1)+".png",bbox_inches='tight',dpi=200)
+plt.savefig(path+"plots/areas-"+str(endYear+1)+".png",bbox_inches='tight',dpi=200)
 
 #%%
 startYear=1980
@@ -80,7 +81,7 @@ plt.ylabel('category of startups')
 plt.savefig("cat-"+str(endYear+1)+".png",bbox_inches='tight',dpi=200)
 
 #%%
-startYear=1980
+startYear=1990
 endYear = 2013
 
 df={}
@@ -103,20 +104,20 @@ df.sort_values(str(endYear),ascending=False,inplace=True)
 df.columns=df.columns.astype(int)
 
 plt.plot(df.T['Software'],label='Software')
-plt.plot(df.T['Mobile'],label='Mobile')
+# plt.plot(df.T['Mobile'],label='Mobile')
 # plt.plot(df.T['Games'],label='Games')
 plt.plot(df.T['Social Media'],label='Social Media')
 plt.plot(df.T['Biotechnology'],label='Biotechnology')
 plt.plot(df.T['Semiconductors'],label='Semiconductors')
 plt.yscale('log')
-plt.xticks(range(1980,2020,10))
+plt.xticks(range(1990,2020,10))
 plt.xlabel('year')
 plt.ylabel('# of startups')
 plt.title('# of some categories of startups over time')
 plt.grid()
 plt.legend()
 
-plt.savefig('over-time.png')
+plt.savefig(path+'/plots/over-time.png')
 
 #%%
 f=data[(1980<data['founded_year'])]
@@ -132,9 +133,9 @@ plt.savefig('invest-startups.png',bbox_inches='tight',dpi=200)
 #%%
 
 data.groupby(['market','founded_year']).agg('sum').funding_total_usd
-startYear=2010
+startYear=2000
 endYear = 2013
-thresh = 0.8e9
+thresh = 2.4e9
 
 df={}
 for year,group in data.groupby('founded_year'):
@@ -148,10 +149,10 @@ print(len(a))
 
 a=np.log(a+1)
 sns.heatmap(a,linewidths=.1)
-plt.title("invest areas of statrtups "+str(startYear)+"-"+str(endYear))
+plt.title("invested $(log) in statrtups in best areas of startups - "+str(startYear)+"-"+str(endYear))
 plt.xlabel('year')
 plt.ylabel('areas of startups')
-plt.savefig("invest-areas-"+str(endYear+1)+".png",bbox_inches='tight',dpi=200)
+plt.savefig(path+"plots/invest-areas-"+str(endYear+1)+".png",bbox_inches='tight',dpi=200)
 
 #%%
 
@@ -187,7 +188,7 @@ plt.ylabel('category of startups')
 plt.savefig("invest-cat-"+str(endYear+1)+".png",bbox_inches='tight',dpi=200)
 
 #%%
-startYear=1980
+startYear=1990
 endYear = 2013
 
 df={}
@@ -210,20 +211,20 @@ df.sort_values(str(endYear),ascending=False,inplace=True)
 df.columns=df.columns.astype(int)
 
 plt.plot(df.T['Software'],label='Software')
-plt.plot(df.T['Mobile'],label='Mobile')
+# plt.plot(df.T['Mobile'],label='Mobile')
 # plt.plot(df.T['Games'],label='Games')
 plt.plot(df.T['Social Media'],label='Social Media')
 plt.plot(df.T['Biotechnology'],label='Biotechnology')
 plt.plot(df.T['Semiconductors'],label='Semiconductors')
 plt.yscale('log')
-plt.xticks(range(1980,2020,10))
+plt.xticks(range(1990,2020,10))
 plt.xlabel('year')
 plt.ylabel('invest on startups')
 plt.title('invest on some categories of startups over time')
 plt.grid()
 plt.legend()
 
-plt.savefig('invest-over-time.png')
+plt.savefig(path+'/plots/invest-over-time.png')
 
 #%%
 a=data.groupby("country_code").name.count().sort_values(ascending=False)[:10]
@@ -232,7 +233,16 @@ plt.yscale('log')
 plt.title('# of startups in each country')
 plt.xlabel('country')
 plt.ylabel('# of startups')
-plt.savefig(".png",bbox_inches='tight',dpi=200)
+plt.savefig(path+"plots/countries.png",bbox_inches='tight',dpi=200)
+
+#%%
+a=data.groupby("status").name.count().sort_values(ascending=False)[:10]
+plt.bar(a.index,a)
+# plt.yscale('log')
+plt.title('status of startups')
+plt.xlabel('status',fontsize='large', fontweight='bold')
+plt.ylabel('count',fontsize='large', fontweight='bold')
+plt.savefig(path+"plots/status.png",bbox_inches='tight',dpi=200)
 
 #%%
 # d=data.dropna(subset=['status'])
@@ -286,4 +296,57 @@ fig.update_layout(
     ,yaxis_title='success rate')
 plotly.offline.plot(fig)
 
+#%%
+startYear=1980
+endYear = 2013
 
+df_inv={}
+for i in range(startYear,endYear+1):
+    df_inv[str(i)]={}
+filtered = data[(data.founded_year>=startYear) & (data.founded_year<=endYear)]
+for d,y,im in zip(filtered['category_list'],filtered['founded_year'],filtered["funding_total_usd"]):
+    y=str(y)
+    if type(d)!=str:
+        continue
+    for i in d.split('|'):
+        if i in df_inv[y]:
+            df_inv[y][i]+=im
+        elif i!='':
+            df_inv[y][i]=im
+
+df_inv=pd.DataFrame(df_inv)
+df_inv.fillna(0,inplace=True)
+df_inv.sort_values(str(endYear),ascending=False,inplace=True)
+df_inv.columns=df_inv.columns.astype(int)
+
+
+df_count={}
+for i in range(startYear,endYear+1):
+    df_count[str(i)]={}
+filtered = data[(data.founded_year>=startYear) & (data.founded_year<=endYear)]
+for d,y in zip(filtered['category_list'],filtered['founded_year']):
+    y=str(y)
+    if type(d)!=str:
+        continue
+    for i in d.split('|'):
+        if i in df_count[y]:
+            df_count[y][i]+=1
+        elif i!='':
+            df_count[y][i]=1
+
+df_count=pd.DataFrame(df_count)
+df_count.fillna(0,inplace=True)
+df_count.sort_values(str(endYear),ascending=False,inplace=True)
+df_count.columns=df_count.columns.astype(int)
+
+
+for i in ['Semiconductors','Biotechnology','Social Media','Mobile','Software']:
+    plt.plot(df_inv.T[i],label='invest')
+    plt.plot(1e7*df_count.T[i],label='count')
+    plt.title(i)
+    plt.legend()
+    plt.yscale('log')
+    plt.yticks([])
+    plt.grid(axis='x')
+    plt.savefig(path+'/plots/'+i+'.png')
+    plt.close()
